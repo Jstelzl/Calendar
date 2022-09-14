70930f6715753f855a409b9fb0d7cb43fa534286
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-09-14 15:52:56 -0400

    added time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 function start() {
-    for (var time = 0; time < 24; time++) {
+    var currentHour = moment().format('h a');
+    console.log(currentHour);
+    for (var time = 9; time < 19; time++) {
     var id = time;
     var calendarRow = $("<form>")
             .addClass("row");
@@ -8,17 +10,24 @@ function start() {
 
     var timeBlock = $("<div>")
             .addClass("col-1 hour");
+            timeBlock.text(time);
         var notes = $("<textarea>")
             .addClass("full col-10");
+            if (time > currentHour) notes.addClass("future");
+            if (time < currentHour) notes.addClass("past");
+            if (time === currentHour) notes.addClass("present");
 
     var submitBtn = $("<button>")
             .addClass("col-1 saveBtn");
-        var saveIcon = $("<i class='bi bi-save2-fill'></i>") 
+        var saveIcon = $("<i class='bi bi-save2-fill'>Submit</i>") 
         submitBtn.append(saveIcon);
   
         calendarRow.append(timeBlock, notes, submitBtn);    
-    console.log(id);
+        console.log(id);
     }
-}
+    
+};
+document.getElementById('currentDay').textContent = moment().format('MMMM Do YYYY');
+console.log(moment().format('MMMM Do YYYY, h:mm a'));
 start();
 
